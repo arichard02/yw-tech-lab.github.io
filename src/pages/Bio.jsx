7aa633e14a50414634ddc9d01d883085f1065596
Adwaina <arichard02@aol.com>
2025-06-03 10:41:38 -0400

    Add Bio

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -9,12 +9,14 @@ const Bio = () => {
           <h2 className="md:col-span-2">About Me</h2>
           <div>
             <p className="body-text">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien
-              vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus
-              leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus
-              bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
-              hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
-              nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
+              I'm a front-end web developer with a passion for solving complex problems and turning
+              ideas into intuitive, functional websites. My background in customer support and
+              healthcare taught me to stay curious, think critically, and always focus on helping
+              people — skills I now bring to every project I code. I love using both logic and
+              creativity to build user-centered products that make a real impact. My process is
+              grounded in research, collaboration, and a commitment to continuous learning. I'm
+              excited to contribute to teams that value thoughtful design, clear communication, and
+              purposeful development.
             </p>
             <p className="body-text">
               Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
